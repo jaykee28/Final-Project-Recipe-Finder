b0badc54966830f578888c7433b4bffdfbc8b995
jaykee28 <jmufetu@gmail.com>
2026-04-16 22:30:58 +0200

    Fixed paths

---
+++ dist/assets/favorites-fbLup9-Q.js
@@ -0,0 +1,5 @@
+import{l as i}from"./utils-BBBZukLT.js";i();const a=document.getElementById("favorites");let r=JSON.parse(localStorage.getItem("favorites"))||[];r.length===0?a.innerHTML="<p>No favorites yet 😔</p>":r.forEach(t=>{const e=document.createElement("div");e.classList.add("favorite-card"),e.innerHTML=`
+      <img src="${t.image}" alt="${t.name}" />
+      <h3>${t.name}</h3>
+      <button class="remove-btn" data-id="${t.idMeal}">❌ Remove</button>
+    `,e.querySelector(".remove-btn").addEventListener("click",o=>{o.stopPropagation(),n(t.idMeal),e.remove()}),a.appendChild(e)});function n(t){let e=JSON.parse(localStorage.getItem("favorites"))||[];e=e.filter(o=>o.idMeal!==t),localStorage.setItem("favorites",JSON.stringify(e))}
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
-export default {
-  base: '/Final-Project-Recipe-Finder/'
-};
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Final-Project-Recipe-Finder/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        favorites: 'favorites.html'
+      }
+    }
+  }
+});
